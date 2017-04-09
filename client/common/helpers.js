2ff9bd288ffd606ff5028710e7e182ee376ff8e8
abhishek <akmnitt@gmail.com>
2017-04-09 09:29:09 +0530

    no limit display

--- client/common/helpers.js
+++ client/common/helpers.js
@@ -1,21 +1,3 @@
-Template.registerHelper("showmorecodebutton", function(running) {
-    var id = Meteor.userId();
-
-    if (Roles.userIsInRole(id, 'two-ride-seller')) {
-        return running < 2;
-    } else if (Roles.userIsInRole(id, 'ten-ride-seller')) {
-        return running < 10;
-    } else if (Roles.userIsInRole(id, 'twenty-ride-seller')) {
-        return running < 20;
-    } else if (Roles.userIsInRole(id, 'premium-seller')) {
-        return running < 50;
-    } else if (Roles.userIsInRole(id, 'admin')) {
-        return running < 1000;
-    }
-    return false;
-
-});
-
 Template.registerHelper('formatDate', function(date) {
     return moment(date).format('MMMM Do hh:mm A');
 });
