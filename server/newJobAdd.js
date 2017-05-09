46eec7b435835b6b3c97028ab064cb075d435d88
abhishek <akmnitt@gmail.com>
2017-05-09 08:37:31 +0530

    limit change

--- server/newJobAdd.js
+++ server/newJobAdd.js
@@ -1,354 +1,354 @@
 // first job will be assigned daily after midnight , if its not running.
 var idsFirstJob = function(dayStart) {
-    let ids = [];
-    Jobs.find({
-        deleted: false,
-        $or: [{
-            createdAt: {
-                $gt: dayStart
-            }
-        }, {
-            done: false
-        }]
-    }).map(function(o) {
-        ids.push(o.orderId);
-    });
-    //  console.log(ids);
-    return ids;
+  let ids = [];
+  Jobs.find({
+    deleted: false,
+    $or: [{
+      createdAt: {
+        $gt: dayStart
+      }
+    }, {
+      done: false
+    }]
+  }).map(function(o) {
+    ids.push(o.orderId);
+  });
+  //  console.log(ids);
+  return ids;
 };
 // second job will be assigned after 16 hours of ending last first/second job.
 var idsSecondJob = function(dayStart) {
-    ids = [];
-    Jobs.find({
-        deleted: false,
-        $or: [{
-            createdAt: {
-                $gt: dayStart
-            }
-        }, {
-            done: false
-        }]
-    }).map(function(o) {
-        var assignedcount = Jobs.find({
-            orderId: o.orderId,
-            deleted: false,
-            $or: [{
-                createdAt: {
-                    $gt: dayStart
-                }
-            }, {
-                done: false
-            }]
-        }).count();
-        //    console.log('assignedcount: ' + assignedcount);
-        //    console.log(o.code);
-        if (assignedcount > 1) ids.push(o.orderId);
-    });
-    return ids;
+  ids = [];
+  Jobs.find({
+    deleted: false,
+    $or: [{
+      createdAt: {
+        $gt: dayStart
+      }
+    }, {
+      done: false
+    }]
+  }).map(function(o) {
+    var assignedcount = Jobs.find({
+      orderId: o.orderId,
+      deleted: false,
+      $or: [{
+        createdAt: {
+          $gt: dayStart
+        }
+      }, {
+        done: false
+      }]
+    }).count();
+    //    console.log('assignedcount: ' + assignedcount);
+    //    console.log(o.code);
+    if (assignedcount > 1) ids.push(o.orderId);
+  });
+  return ids;
 };
 
 var jobcount = function(ordr, count, adderId, dayStart) {
-    var jobcount = 0;
-    //don't allow to get added by same person in same day
-    var countJ = Jobs.find({
-        adderId: adderId,
-        deleted: false,
-        orderId: ordr._id,
-        createdAt: {
-            $gte: dayStart
-        }
-    }).count();
-    //console.log('J1 ' + countJ);
-    //don't allow to get it added by any person in within 4 hours.
-    //  console.log(ordr.code);
-    Jobs.find({
-        orderId: ordr._id,
-        done: true,
-        deleted: false,
-        updatedAt: {
-            $gte: dayStart
-        }
-    }).map(function(jb) {
-        var ud = new Date() - jb.updatedAt;
-        if (ud < 1000 * 60 * 60 * 4) countJ++;
-        //    console.log(ud);
-    });
-    //  console.log('j2 ' + countJ);
-    if (countJ === 0) {
-        // to avoid adding extra rides then ordered
-        var addedForCode = 0;
-        Jobs.find({
-            orderId: ordr._id,
-            deleted: false
-        }).map(function(jb) {
-            addedForCode = addedForCode + jb.count;
-        });
-        if (ordr.rides - addedForCode === 1 && count > 1) {
-            jobcount = 1;
-        } else if (ordr.rides - addedForCode > 1 && count > 1) {
-            jobcount = 2;
-        }
-        //    console.log('ordr.added ' + ordr.added);
-        //    console.log('addedForCode ' + addedForCode);
-        //    console.log('jobcount ' + jobcount);
-        if (jobcount > 0) {
-            Meteor.call('jobs.insert', ordr._id, ordr.code, ordr.premium, jobcount, adderId, (error, result) => {
-                if (error) {
-                    //    console.log(error);
-                    jobcount = 0;
-                }
-                if (result) {
-                    //    console.log("result " + result);
-                    //    console.log(jobcount);
-                    //return jobcount;
-                }
-            });
-        }
+  var jobcount = 0;
+  //don't allow to get added by same person in same day
+  var countJ = Jobs.find({
+    adderId: adderId,
+    deleted: false,
+    orderId: ordr._id,
+    createdAt: {
+      $gte: dayStart
     }
-    return jobcount;
+  }).count();
+  //console.log('J1 ' + countJ);
+  //don't allow to get it added by any person in within 4 hours.
+  //  console.log(ordr.code);
+  Jobs.find({
+    orderId: ordr._id,
+    done: true,
+    deleted: false,
+    updatedAt: {
+      $gte: dayStart
+    }
+  }).map(function(jb) {
+    var ud = new Date() - jb.updatedAt;
+    if (ud < 1000 * 60 * 60 * 4) countJ++;
+    //    console.log(ud);
+  });
+  //  console.log('j2 ' + countJ);
+  if (countJ === 0) {
+    // to avoid adding extra rides then ordered
+    var addedForCode = 0;
+    Jobs.find({
+      orderId: ordr._id,
+      deleted: false
+    }).map(function(jb) {
+      addedForCode = addedForCode + jb.count;
+    });
+    if (ordr.rides - addedForCode === 1 && count > 1) {
+      jobcount = 1;
+    } else if (ordr.rides - addedForCode > 1 && count > 1) {
+      jobcount = 2;
+    }
+    //    console.log('ordr.added ' + ordr.added);
+    //    console.log('addedForCode ' + addedForCode);
+    //    console.log('jobcount ' + jobcount);
+    if (jobcount > 0) {
+      Meteor.call('jobs.insert', ordr._id, ordr.code, ordr.premium, jobcount, adderId, (error, result) => {
+        if (error) {
+          //    console.log(error);
+          jobcount = 0;
+        }
+        if (result) {
+          //    console.log("result " + result);
+          //    console.log(jobcount);
+          //return jobcount;
+        }
+      });
+    }
+  }
+  return jobcount;
 }
 
 var allowedRides = function(id) {
-    if (Roles.userIsInRole(id, 'two-ride-seller')) {
-        return 2;
-    } else if (Roles.userIsInRole(id, 'ten-ride-seller')) {
-        return 10;
-    } else if (Roles.userIsInRole(id, 'twenty-ride-seller')) {
-        return 20;
-    } else if (Roles.userIsInRole(id, 'premium-seller')) {
-        return 80;
-    } else if (Roles.userIsInRole(id, 'admin')) {
-        return 1000;
-    }
-    return 0;
+  if (Roles.userIsInRole(id, 'two-ride-seller')) {
+    return 2;
+  } else if (Roles.userIsInRole(id, 'ten-ride-seller')) {
+    return 10;
+  } else if (Roles.userIsInRole(id, 'twenty-ride-seller')) {
+    return 20;
+  } else if (Roles.userIsInRole(id, 'premium-seller')) {
+    return 100;
+  } else if (Roles.userIsInRole(id, 'admin')) {
+    return 1000;
+  }
+  return 0;
 };
 var sellerDailyLimit = function(id) {
-    if (Roles.userIsInRole(id, 'two-ride-seller')) {
-        return 10;
-    } else if (Roles.userIsInRole(id, 'ten-ride-seller')) {
-        return 30;
-    } else if (Roles.userIsInRole(id, 'twenty-ride-seller')) {
-        return 100;
-    } else if (Roles.userIsInRole(id, 'premium-seller')) {
-        return 120;
-    } else if (Roles.userIsInRole(id, 'admin')) {
-        return 1000;
-    }
-    return 0;
+  if (Roles.userIsInRole(id, 'two-ride-seller')) {
+    return 10;
+  } else if (Roles.userIsInRole(id, 'ten-ride-seller')) {
+    return 30;
+  } else if (Roles.userIsInRole(id, 'twenty-ride-seller')) {
+    return 100;
+  } else if (Roles.userIsInRole(id, 'premium-seller')) {
+    return 200;
+  } else if (Roles.userIsInRole(id, 'admin')) {
+    return 1000;
+  }
+  return 0;
 };
 
 
 var userRunningRides = function(adderId) {
-    //  console.log('adderId ' + adderId);
-    var total = 0;
-    Jobs.find({
-        adderId: adderId,
-        done: false,
-        deleted: false
-    }).map(function(job) {
-        total = total + job.count;
-    });
-    return total;
+  //  console.log('adderId ' + adderId);
+  var total = 0;
+  Jobs.find({
+    adderId: adderId,
+    done: false,
+    deleted: false
+  }).map(function(job) {
+    total = total + job.count;
+  });
+  return total;
 }
 
 var userDailyRunningRidesCount = function(adderId, dayStart) {
-    //  console.log('adderId ' + adderId);
-    var total = 0;
-    Jobs.find({
-        adderId: adderId,
-        deleted: false,
-        createdAt: {
-            $gt: dayStart
-        }
-    }).map(function(job) {
-        total = total + job.count;
-    });
-    return total;
+  //  console.log('adderId ' + adderId);
+  var total = 0;
+  Jobs.find({
+    adderId: adderId,
+    deleted: false,
+    createdAt: {
+      $gt: dayStart
+    }
+  }).map(function(job) {
+    total = total + job.count;
+  });
+  return total;
 }
 
 Meteor.methods({
-    "addJobOrder": function(adderId, count, dayStart) {
-        //    console.log('dayStart');
-        //    console.log(dayStart);
-        if (!dayStart || !adderId) return; // return if undefined
-        //    console.log('count: ' + count);
-        if (!Roles.userIsInRole(adderId, ['admin', 'seller'])) return;
-        //    console.log('allowed ' + allowedRides(adderId));
-        //    console.log('userrun ' + userRunningRides(adderId));
-        //    console.log('limit ' + sellerDailyLimit(adderId));
-        //    console.log('userdailyrun ' + userDailyRunningRidesCount(adderId, dayStart));
-        if (allowedRides(adderId) <= userRunningRides(adderId)) return;
-        if (sellerDailyLimit(adderId) <= userDailyRunningRidesCount(adderId, dayStart)) return;
-        if (count < 1) return;
-        //    console.log('adding now');
-        // console.log('ids1');
-        //    console.log(idsFirstJob(dayStart));
-        // console.log('ids2');
-        // console.log(idsSecondJob(dayStart));
-        //    console.log('1: ' + count);
-        Orders.find({
-            _id: {
-                $nin: idsFirstJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            added: 0,
-            premium: true,
-            deleted: false,
-            $where: "this.rides - this.added > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
-        //currentListPremium
-        //    console.log('2: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsFirstJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            deleted: false,
-            added: {
-                $gt: 0
-            },
-            premium: true,
-            $where: "this.rides - this.added > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
-        //    currentListNormalNew: function() {
-        //      console.log('3: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsFirstJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            added: 0,
-            premium: false,
-            deleted: false,
-            $where: "this.rides - this.added > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
+  "addJobOrder": function(adderId, count, dayStart) {
+    //    console.log('dayStart');
+    //    console.log(dayStart);
+    if (!dayStart || !adderId) return; // return if undefined
+    //    console.log('count: ' + count);
+    if (!Roles.userIsInRole(adderId, ['admin', 'seller'])) return;
+    //    console.log('allowed ' + allowedRides(adderId));
+    //    console.log('userrun ' + userRunningRides(adderId));
+    //    console.log('limit ' + sellerDailyLimit(adderId));
+    //    console.log('userdailyrun ' + userDailyRunningRidesCount(adderId, dayStart));
+    if (allowedRides(adderId) <= userRunningRides(adderId)) return;
+    if (sellerDailyLimit(adderId) <= userDailyRunningRidesCount(adderId, dayStart)) return;
+    if (count < 1) return;
+    //    console.log('adding now');
+    // console.log('ids1');
+    //    console.log(idsFirstJob(dayStart));
+    // console.log('ids2');
+    // console.log(idsSecondJob(dayStart));
+    //    console.log('1: ' + count);
+    Orders.find({
+      _id: {
+        $nin: idsFirstJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      added: 0,
+      premium: true,
+      deleted: false,
+      $where: "this.rides - this.added > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
+    //currentListPremium
+    //    console.log('2: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsFirstJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      deleted: false,
+      added: {
+        $gt: 0
+      },
+      premium: true,
+      $where: "this.rides - this.added > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
+    //    currentListNormalNew: function() {
+    //      console.log('3: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsFirstJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      added: 0,
+      premium: false,
+      deleted: false,
+      $where: "this.rides - this.added > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
 
-        //  currentListNormal
-        //    console.log('4: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsFirstJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            deleted: false,
-            added: {
-                $gt: 0
-            },
-            premium: false,
-            $where: "this.rides - this.added > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
+    //  currentListNormal
+    //    console.log('4: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsFirstJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      deleted: false,
+      added: {
+        $gt: 0
+      },
+      premium: false,
+      $where: "this.rides - this.added > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
 
-        //      currentListOneRide
-        //    console.log('5: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsFirstJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            deleted: false,
-            $where: "this.rides - this.added === 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
-        //code repeat if all done for first round
-        //currentListPremium
-        //      console.log('6: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsSecondJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            premium: true,
-            runStatus: false,
-            deleted: false,
-            $where: "this.rides - this.added -2 > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
+    //      currentListOneRide
+    //    console.log('5: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsFirstJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      deleted: false,
+      $where: "this.rides - this.added === 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
+    //code repeat if all done for first round
+    //currentListPremium
+    //      console.log('6: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsSecondJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      premium: true,
+      runStatus: false,
+      deleted: false,
+      $where: "this.rides - this.added -2 > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
 
-        //  currentListNormal
-        //      console.log('7: ' + count);
-        if (count < 1) return;
-        Orders.find({
-            _id: {
-                $nin: idsSecondJob(dayStart)
-            },
-            done: false,
-            pause: false,
-            premium: false,
-            runStatus: false,
-            deleted: false,
-            $where: "this.rides - this.added - 2 > 1"
-        }, {
-            sort: {
-                createdAt: 1
-            }
-        }).map(function(ordr) {
-            if (count > 0) {
-                var jc = jobcount(ordr, count, adderId, dayStart);
-                count = count - jc;
-            }
-        });
-    }
+    //  currentListNormal
+    //      console.log('7: ' + count);
+    if (count < 1) return;
+    Orders.find({
+      _id: {
+        $nin: idsSecondJob(dayStart)
+      },
+      done: false,
+      pause: false,
+      premium: false,
+      runStatus: false,
+      deleted: false,
+      $where: "this.rides - this.added - 2 > 1"
+    }, {
+      sort: {
+        createdAt: 1
+      }
+    }).map(function(ordr) {
+      if (count > 0) {
+        var jc = jobcount(ordr, count, adderId, dayStart);
+        count = count - jc;
+      }
+    });
+  }
 });
--- server/users-publish.js
+++ server/users-publish.js
@@ -1,163 +1,163 @@
 Meteor.publish('allUsers', function(search) {
-    check(search, Match.OneOf(String, null, undefined));
-    let options = {
-        sort: {
-            createdAt: -1
-        },
-        fields: {
-            emails: 1,
-            roles: 1,
-            profile: 1,
-            createdAt: 1,
-            userCode: 1,
-            firstOrder: 1
-        },
-        limit: 5
-    };
-    if (search === '') {
-        if (Roles.userIsInRole(this.userId, 'admin')) {
-            return Meteor.users.find({}, options);
-        }
-    } else if (search === 'users') {
-        if (Roles.userIsInRole(this.userId, 'admin')) {
-            return Meteor.users.find({}, options);
-        }
-    } else if (search === 'sellers') {
-        if (Roles.userIsInRole(this.userId, 'admin')) {
-            return Meteor.users.find({
-                roles: 'seller'
-            }, options);
-        }
-    } else if (search === 'buyers') {
-        if (Roles.userIsInRole(this.userId, 'admin')) {
-            return Meteor.users.find({
-                roles: 'buyer'
-            }, options);
-        }
-    } else {
-        let query = {};
-        if (search) {
-            let regex = new RegExp(search, 'i');
-
-            query = {
-                $or: [{
-                        "emails.address": regex
-                    },
-                    {
-                        "profile.firstName": regex
-                    }
-                ]
-            };
-        }
-        if (Roles.userIsInRole(this.userId, 'admin'))
-            return Meteor.users.find(query, options);
+  check(search, Match.OneOf(String, null, undefined));
+  let options = {
+    sort: {
+      createdAt: -1
+    },
+    fields: {
+      emails: 1,
+      roles: 1,
+      profile: 1,
+      createdAt: 1,
+      userCode: 1,
+      firstOrder: 1
+    },
+    limit: 20
+  };
+  if (search === '') {
+    if (Roles.userIsInRole(this.userId, 'admin')) {
+      return Meteor.users.find({}, options);
     }
+  } else if (search === 'users') {
+    if (Roles.userIsInRole(this.userId, 'admin')) {
+      return Meteor.users.find({}, options);
+    }
+  } else if (search === 'sellers') {
+    if (Roles.userIsInRole(this.userId, 'admin')) {
+      return Meteor.users.find({
+        roles: 'seller'
+      }, options);
+    }
+  } else if (search === 'buyers') {
+    if (Roles.userIsInRole(this.userId, 'admin')) {
+      return Meteor.users.find({
+        roles: 'buyer'
+      }, options);
+    }
+  } else {
+    let query = {};
+    if (search) {
+      let regex = new RegExp(search, 'i');
+
+      query = {
+        $or: [{
+            "emails.address": regex
+          },
+          {
+            "profile.firstName": regex
+          }
+        ]
+      };
+    }
+    if (Roles.userIsInRole(this.userId, 'admin'))
+      return Meteor.users.find(query, options);
+  }
 });
 
 Meteor.publish('allBuyers', function(search) {
-    check(search, Match.OneOf(String, null, undefined));
-    let query = {};
-    if (search) {
-        let regex = new RegExp(search, 'i');
+  check(search, Match.OneOf(String, null, undefined));
+  let query = {};
+  if (search) {
+    let regex = new RegExp(search, 'i');
 
-        query = {
-            roles: 'buyer',
-            $or: [{
-                    "emails.address": regex
-                },
-                {
-                    "profile.firstName": regex
-                }
-            ]
-        };
-    }
-    if (Roles.userIsInRole(this.userId, 'admin')) {
-        return Meteor.users.find(query, {
-            sort: {
-                createdAt: -1
-            },
-            fields: {
-                emails: 1,
-                roles: 1,
-                profile: 1,
-                createdAt: 1,
-                userCode: 1,
-                firstOrder: 1
-            },
-            limit: 5
-        });
-    }
+    query = {
+      roles: 'buyer',
+      $or: [{
+          "emails.address": regex
+        },
+        {
+          "profile.firstName": regex
+        }
+      ]
+    };
+  }
+  if (Roles.userIsInRole(this.userId, 'admin')) {
+    return Meteor.users.find(query, {
+      sort: {
+        createdAt: -1
+      },
+      fields: {
+        emails: 1,
+        roles: 1,
+        profile: 1,
+        createdAt: 1,
+        userCode: 1,
+        firstOrder: 1
+      },
+      limit: 10
+    });
+  }
 });
 Meteor.publish('jobAdders', function(orderId) {
-    if (Roles.userIsInRole(this.userId, 'admin')) {
-        var ids = [];
-        Jobs.find({
-            orderId: orderId,
-            deleted: false
-        }).map(function(jb) {
-            ids.push(jb.adderId);
-        });
-        ids.push(Orders.findOne({
-            _id: orderId
-        }).userId);
-        var options = {
-            fields: {
-                emails: 1,
-                profile: 1,
-                createdAt: 1,
-                roles: 1
-            }
-        };
-        return Meteor.users.find({
-            _id: {
-                $in: ids
-            }
-        }, options);
-    }
+  if (Roles.userIsInRole(this.userId, 'admin')) {
+    var ids = [];
+    Jobs.find({
+      orderId: orderId,
+      deleted: false
+    }).map(function(jb) {
+      ids.push(jb.adderId);
+    });
+    ids.push(Orders.findOne({
+      _id: orderId
+    }).userId);
+    var options = {
+      fields: {
+        emails: 1,
+        profile: 1,
+        createdAt: 1,
+        roles: 1
+      }
+    };
+    return Meteor.users.find({
+      _id: {
+        $in: ids
+      }
+    }, options);
+  }
 });
 
 
 Meteor.publish('allSellers', function() {
-    if (Roles.userIsInRole(this.userId, 'admin')) {
-        var options = {
-            fields: {
-                emails: 1
-            }
-        };
-        return Meteor.users.find({
-            roles: 'seller'
-        }, options);
-    }
+  if (Roles.userIsInRole(this.userId, 'admin')) {
+    var options = {
+      fields: {
+        emails: 1
+      }
+    };
+    return Meteor.users.find({
+      roles: 'seller'
+    }, options);
+  }
 });
 
 Meteor.publish('profile', function(id) {
-    let options = {
-        fields: {
-            emails: 1,
-            roles: 1,
-            profile: 1,
-            createdAt: 1,
-            userCode: 1,
-            firstOrder: 1
-        }
-    };
-    if (Roles.userIsInRole(id, 'buyer')) {
-        var code = Meteor.users.findOne({
-            _id: id
-        }).userCode;
-        //    console.log('in buyer');
-        return Meteor.users.find({
-            $or: [{
-                    _id: id
-                },
-                {
-                    "profile.refCodeBy": code
-                }
-            ]
-        }, options);
-    } else {
-        return Meteor.users.find({
-            _id: id
-        }, options);
+  let options = {
+    fields: {
+      emails: 1,
+      roles: 1,
+      profile: 1,
+      createdAt: 1,
+      userCode: 1,
+      firstOrder: 1
     }
+  };
+  if (Roles.userIsInRole(id, 'buyer')) {
+    var code = Meteor.users.findOne({
+      _id: id
+    }).userCode;
+    //    console.log('in buyer');
+    return Meteor.users.find({
+      $or: [{
+          _id: id
+        },
+        {
+          "profile.refCodeBy": code
+        }
+      ]
+    }, options);
+  } else {
+    return Meteor.users.find({
+      _id: id
+    }, options);
+  }
 });
