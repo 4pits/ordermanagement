49b3129b3a44f43b138c6ac63679ff8f41e15b75
abhishek <akmnitt@gmail.com>
2017-06-15 08:46:21 +0530

    admin time

--- client/common/helpers.js
+++ client/common/helpers.js
@@ -1,11 +1,23 @@
 Template.registerHelper('formatDate', function(date) {
+  if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
+    return moment(date).format('MMMM Do hh:mm:ss.SSS a');
+  } else {
     return moment(date).format('MMMM Do hh:mm A');
+  }
 });
 
 Template.registerHelper('formatDay', function(date) {
+  if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
     return moment(date).format('MMMM Do YYYY');
+  } else {
+    return moment(date).format('MMMM Do YYYY');
+  }
 });
 
 Template.registerHelper('formatDay2', function(date) {
+  if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
     return moment(date).format('MMM Do');
+  } else {
+    return moment(date).format('MMM Do');
+  }
 });
